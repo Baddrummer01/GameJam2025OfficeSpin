8963e03dc84a0a900fc7ce817f78f1cd3e10e90a
Baddrummer01 <bennettstarks@gmail.com>
2025-01-09 02:16:11 -0500

    1/9/2025 update.
    Healthbar, spin controls, new animations, and damage interactions complete

--- bossrushgamejam2025-2d/generalscenes/chair_player.gd
+++ bossrushgamejam2025-2d/generalscenes/chair_player.gd
@@ -7,21 +7,30 @@ const SPEED = 300.0
 @onready var spin_timer: Timer = $SpinTimer
 @onready var attack_timer: Timer = $AttackTimer
 @onready var rotation_bar: TextureProgressBar = $"../PlayerHealthbars/RotationBar"
+@onready var end_boost_timer: Timer = $EndBoostTimer
+@onready var stamina_bar_timer: Timer = $StaminaBarTimer
+@onready var invuln_frames: Timer = $InvulnFrames
+@onready var mainbar_health: TextureProgressBar = $"../PlayerHealthbars/Mainbar+health"
+
 
 var spinState = 0
 var last_input_vector = Vector2.ZERO
-
+var isSpinning = false
 
 func _physics_process(_delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	
+	#should enter if boost has just ended for ramping down speed
+	if !end_boost_timer.is_stopped():
+		velocity = last_input_vector * SPEED
 	#should enter if player releases in the last state OR if the attack timer is still going
-	if (spinState == 3 && !Input.is_action_pressed("spin_player")) || !attack_timer.is_stopped():
+	elif (spinState == 3 && !Input.is_action_pressed("spin_player")) || !attack_timer.is_stopped():
 		spin_timer.stop()
 		if (attack_timer.is_stopped()):
 			attack_timer.start()
+		chair_player.animation = "boost_mode"
 			
 		# Use the last input vector for continuous movement
 		if last_input_vector != Vector2.ZERO:
@@ -38,27 +47,33 @@ func _physics_process(_delta: float) -> void:
 		
 	else:
 		#Animations
-		if Input.is_action_pressed("spin_player"):
-			if spin_timer.is_stopped():
-				spin_timer.start()
-			if spinState == 0:
-				chair_player.animation = "spin_low"
-			elif spinState == 1:
-				chair_player.animation = "spin_med"
+		if invuln_frames.is_stopped():
+			if Input.is_action_pressed("spin_player"):
+				if spin_timer.is_stopped():
+					spin_timer.start()
+				if spinState == 0:
+					chair_player.animation = "spin_low"
+				elif spinState == 1:
+					chair_player.animation = "spin_med"
+				elif spinState == 2:
+					chair_player.animation = "spin_almost"
+				else:
+					chair_player.animation = "spin_high"
+			elif velocity.x < 0:
+				chair_player.animation = "move_left"
+			elif velocity.x > 0:
+				chair_player.animation = "move_right"
+			elif velocity.y < 0:
+				chair_player.animation = "move_up"
 			else:
-				chair_player.animation = "spin_high"
-		elif velocity.x < 0:
-			chair_player.animation = "move_left"
-		elif velocity.x > 0:
-			chair_player.animation = "move_right"
-		elif velocity.y < 0:
-			chair_player.animation = "move_up"
-		else:
-			chair_player.animation = "default"
+				chair_player.animation = "default"
 		
 		if not Input.is_action_pressed("spin_player"):
 			spinState = 0
 			spin_timer.stop()
+			rotation_bar.value = 0
+			rotation_bar.tint_progress = Color(0, 255, 0)
+		
 		var input_vector := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 		# spinState * 100 adds speed when the spinState escalates
 		if input_vector != Vector2.ZERO:
@@ -70,17 +85,45 @@ func _physics_process(_delta: float) -> void:
 func _on_spin_timer_timeout() -> void:
 	if spinState == 0:
 		spinState = 1
-		rotation_bar.value = 33
+		# rotation_bar.value = 33
 	elif spinState == 1:
 		spinState = 2
-		rotation_bar.value = 66
+		# rotation_bar.value = 66
 	elif spinState == 2:
 		spinState = 3
 		rotation_bar.value = 100
+		rotation_bar.tint_progress = Color(255, 165, 0)
 
 
 func _on_attack_timer_timeout() -> void:
-	chair_player.animation = "default"
+	chair_player.animation = "spin_low"
 	spinState = 0
 	rotation_bar.value = 0
-	
+	rotation_bar.tint_progress = Color(255, 255, 255)
+	end_boost_timer.start()
+
+
+func _on_end_boost_timer_timeout() -> void:
+	chair_player.animation = "default"
+
+func _on_stamina_bar_timer_timeout() -> void:
+	if spin_timer.is_stopped() && rotation_bar.value > 0:
+		rotation_bar.value -= 1
+	elif !spin_timer.is_stopped() && rotation_bar.value < 100:
+		rotation_bar.value += 1
+	else:
+		pass
+		
+
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if invuln_frames.is_stopped() && attack_timer.is_stopped():
+		mainbar_health.value -= 1 # reduces health
+		invuln_frames.start()
+		spin_timer.stop() # resets the spin
+		rotation_bar.value = 0
+		chair_player.animation = "damage_taken"
+
+
+func _on_invuln_frames_timeout() -> void:
+	chair_player.animation = "default"
--- bossrushgamejam2025-2d/generalscenes/level_1_boss.gd
+++ bossrushgamejam2025-2d/generalscenes/level_1_boss.gd
@@ -1,10 +1,15 @@
 extends RigidBody2D
 
+@onready var texture_progress_bar: TextureProgressBar = $"../BossHealthbars/TextureProgressBar"
+@onready var attack_timer: Timer = $"../chairPlayer/AttackTimer"
+@onready var mainbar_health: TextureProgressBar = $"../PlayerHealthbars/Mainbar+health"
+@onready var invuln_frames: Timer = $"../chairPlayer/InvulnFrames"
+@onready var hit_cooldown: Timer = $HitCooldown
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+@onready var target_anim: AnimatedSprite2D = $TargetAnim
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if hit_cooldown.is_stopped() && !attack_timer.is_stopped():
+		texture_progress_bar.value -= 5
+		hit_cooldown.start()
+		target_anim.animation = "damaged"
